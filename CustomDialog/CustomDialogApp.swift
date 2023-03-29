51b6e3f02ab83ed5fafa0059c90ec69f0bada313
githubuiux <uiuxista@gmail.com>
2023-03-29 15:19:56 +0300

    Initial Commit

---
+++ CustomDialog/CustomDialogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDialogApp.swift
+//  CustomDialog
+//
+//  Created by Levent on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomDialogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
